fb198e3fb97eabb956abd3e191c25e18d104fafe
chris4567890 <starchristian4@hotmail.com>
2024-11-30 20:04:50 +0100

    nand gate
    made a simple nand gate using a simple !return at output

--- untitled/src/main/resources/archetype-resources/src/main/java/App.java
+++ untitled/src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,7 +1,15 @@
+import LogicGates.NotGate;
+
+
 public class App
 {
+
     public static void main( String[] args )
+
     {
-         );
+
+        NotGate notGate = new NotGate(false);
+        
+
     }
 }
